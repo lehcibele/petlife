32579e96951883d9f63071201f5f2ca375cb4f78
Leticia Cibele <leticiacibele.cl@gmail.com>
2025-04-14 21:36:52 -0300

    ajuste no js do menu hamburguer

--- src/js/menu-hamburguer.js
+++ src/js/menu-hamburguer.js
@@ -1,6 +1,7 @@
 const iconAbrirHamburguer = document.querySelector('.icon-menu-hambuguer-abrir');
 const iconFecharHamburguer = document.querySelector('.icon-menu-hambuguer-fechar'); 
 const menuHamburguer = document.querySelector('.menu-hamburguer');
+const itemLinkMenu = document.querySelectorAll('.item-link-menu');
 const overlay = document.getElementById('overlay');
 
 function abrirMenuHamguerguer() { 
@@ -12,6 +13,14 @@ function fecharMenuHamburguer() {
     menuHamburguer.classList.remove('abrir');
     overlay.classList.remove('ativo');
 }
+
+/* Ao clicar em um item do menu hamburguer é fechado o menu e tem um scroll até a seção clicada */
+itemLinkMenu.forEach((item) => {
+    item.addEventListener('click', () => {
+        menuHamburguer.classList.remove('abrir');
+        overlay.classList.remove('ativo');
+    })
+});
 
 iconAbrirHamburguer.addEventListener('click', abrirMenuHamguerguer);
 iconFecharHamburguer.addEventListener('click', fecharMenuHamburguer);
